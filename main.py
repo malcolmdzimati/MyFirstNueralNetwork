e86b91c79a9172df6c5ccda8fa8561960a5ad222
malcolmdzimati <malcolmdzimati@gmail.com>
2022-02-14 23:01:37 +0200

    Standardized database

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ for image, label in tfds.as_numpy(ds):
 #fig = tfds.show_examples(train_ds, train_info)
 
 # Metadata
-m_train = train_info.splits["train"].num_examples
-m_test = test_info.splits["test"].num_examples
+m_train = 300                 #train_info.splits["train"].num_examples: 1027
+m_test = 100                  #test_info.splits["test"].num_examples: 256
 num_px = train_info.features["image"].shape[1]
 
 # Reshape the training ad test examples
@@ -43,15 +43,16 @@ test_set_x_orig = []      #Holds the actual picture value of testing set
 train_set_y= []           #Holds the label value of training set 0 is horse and 1 human
 test_set_y= []            #Holds the label value of testing set 0 is horse and 1 human
 
-for image, label in tfds.as_numpy(train_ds):
+ds = train_ds.take(m_train)
+for image, label in tfds.as_numpy(ds):
     train_set_x_orig.append(image)
     train_set_y.append(label)
 
-for image, label in tfds.as_numpy(test_ds):
+ds = test_ds.take(m_test)
+for image, label in tfds.as_numpy(ds):
     test_set_x_orig.append(image)
     test_set_y.append(label)
 
-    #Vector
 train_set_x_orig = np.array(train_set_x_orig)
 test_set_x_orig = np.array(test_set_x_orig)
 
@@ -62,9 +63,59 @@ test_set_y= np.array(test_set_y)
 train_set_x_flatten = train_set_x_orig.reshape(m_train, -1).T
 test_set_x_flatten = test_set_x_orig.reshape(m_test, -1).T
 
-#Check data shape
+"""Check data shape
 print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
 print ("train_set_y shape: " + str(train_set_y.shape))
 print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
 print ("test_set_y shape: " + str(test_set_y.shape))
 print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
+"""
+
+#Clearing Unused variables
+train_set_x_orig = None
+test_set_x_orig = None
+train_ds = None
+test_ds = None
+train_info = None
+test_info = None
+ds = None
+
+#Standardize dataset
+train_set_x = train_set_x_flatten/255
+test_set_x = test_set_x_flatten/255
+
+"""Sigmoid function <Activation Function>"""
+
+def sigmoid(z):
+    """
+        Compute the sigmoid of z
+
+        Arguments:
+        z -- A scalar or numpy array of any size.
+
+        Return:
+        s -- sigmoid(z)
+        """
+    s = 1/(1+np.exp(-z))
+
+    return s
+
+def initialize_with_zeros(dim):
+    """
+        This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+
+        Argument:
+        dim -- size of the w vector we want (or number of parameters in this case)
+
+        Returns:
+        w -- initialized vector of shape (dim, 1)
+        b -- initialized scalar (corresponds to the bias)
+        """
+    w = np.zeros((dim, 1))
+    b = 0
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+
+    return w, b
+
