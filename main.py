d0cc8b58c850f2ba7e914574ff5802a698edaa5b
malcolmdzimati <malcolmdzimati@gmail.com>
2022-02-20 12:06:28 +0200

    Predict function done

--- main.py
+++ main.py
@@ -135,7 +135,102 @@ def propagate(w, b, X, Y):
         db -- gradient of the loss with respect to b, thus same shape as b
 
         """
+
     m = X.shape[1]
-    return
+
+    # FORWARD PROPAGATION (FROM X TO COST)
+    A = sigmoid(np.dot(w.T, X)+b)                                                           #Compute Activation
+    cost = (-1/m)*np.sum(np.dot(np.log(A), Y.T)+np.dot(np.log(1-A), (1-Y).T))               #Compute Cost
+
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    dw = (1/m)*np.dot(X, (A-Y).T)
+    db = (1/m)*np.sum(A-Y)
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+
+    grads = {
+        "dw": dw,
+        "db": db}
+
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations,learning_rate, print_cost = False):
+    """
+    Arguments:
+    :param w: weights, a numpy array of size (num_px * num_px * 3, 1)
+    :param b: bias, a scalar
+    :param X: data of shape (num_px * num_px * 3, numbers of examples)
+    :param Y: true "label" vector (containing 0 if horse, 1 if human), of shape (1, number of examples)
+    :param num_iterations: number of iterations of the optimization loop
+    :param learning_rate: learning rate of the gradient descent update rule
+    :param print_cost: True to print the loss every 100 steps5
+
+    Returns:
+    :return params: dictionary containing the weights w and bias b
+    :return grads: dictionary containing the gradients of the weights and bias with respect to the cost function
+    :return costs: list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    """
+
+    costs = []
+
+    for i in range(num_iterations):
+
+        # Cost and gradient calculation
+        grads, cost = propagate(w, b, X, Y)
+
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+
+        # update rule
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+
+        # Print the cost every 100 training iterations
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
 
 
+def predict(w, b, X):
+    """
+    Arguments --
+    :param w: weights, a numpy array of size (num_px * num_px *3, 1)
+    :param b: bias, a scalar
+    :param X: data of size (num_px * num_px *3, number of examples)
+
+    Returns --
+    :return Y_prediction: a numpy array (vector) containing all predictions (0/1) for the examples in X
+    """
+
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+
+    # Compute vector "A" predicting the probabilities of a human/horse being present in the picture
+    A = sigmoid(np.dot(w.T, X)+b)
+
+    for i in range(A.shape[1]):
+        # Convert probabilities A[0, i] to actual predictions p[0,1]
+        if (A[0, i] > 0.5):
+            Y_prediction[0, i] = 1
+        else:
+            Y_prediction[0, i] = 0
+
+        assert(Y_prediction.shape == (1, m))
+
+    return Y_prediction
